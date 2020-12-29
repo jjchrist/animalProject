14d0ab80ac0100aa9aaf62d228052d10e791df28
jjchrist <55000084+jjchrist@users.noreply.github.com>
2020-12-29 20:51:58 +0300

    Update canines.h

--- canines.h
+++ canines.h
@@ -3,16 +3,16 @@
 
 class Canines: public Animals
 {
-	std::string _place;
+	std::string _food;
 	int _dangerLevel;
 	const std::string _family = "canines";
 public:
 	Canines() {
-		_place = "unknown"; _dangerLevel = 0;
+		_food = "unknown"; _dangerLevel = 0;
 	}
 	Canines(std::string name, int age, std::string place, int danger);
-	const std::string getPlace();
-	void setPlace(std::string place) { _place = place; }
+	const std::string getFood();
+	void setFood(std::string food) { _food = food; }
 	const int getDanger();
 	void setDanger(int danger) { _dangerLevel = danger; }
 	std::string getFamily() { return _family; }
@@ -20,6 +20,4 @@ public:
 	void print();
 	void edit();
 	void save(std::ofstream& F);
-	//virtual void sort(const Canines& c1, const Canines& c2);
-	//const Canines& sortByDanger(const Canines& c1, const Canines& c2);
 };

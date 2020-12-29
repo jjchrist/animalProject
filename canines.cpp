2d3fa09da758e4925265652447ea9391558e8a33
jjchrist <55000084+jjchrist@users.noreply.github.com>
2020-12-29 20:53:03 +0300

    Update canines.cpp

--- canines.cpp
+++ canines.cpp
@@ -1,14 +1,14 @@
 #include "canines.h"
 
-Canines::Canines(std::string name, int age, std::string place, int danger): Animals(name, age)
+Canines::Canines(std::string name, int age, std::string food, int danger): Animals(name, age)
 {
-    _place = place;
+    _food = food;
     _dangerLevel = danger;
 }
 
-const std::string Canines::getPlace()
+const std::string Canines::getFood()
 {
-    return _place;
+    return _food;
 }
 
 const int Canines::getDanger()
@@ -19,22 +19,22 @@ const int Canines::getDanger()
 void Canines::print()
 {
     printAnimals();
-    std::cout << " place: " << getPlace() << "\t\tlevel of danger: " << getDanger() << std::endl;
+    std::cout << " food: " << getFood() << "\t\tlevel of danger: " << getDanger() << std::endl;
 }
 
 void Canines::edit()
 {
     int c = 0;
-    std::cout << "\nEdit:\t1 - place\n\t2 - danger\n\tEnter: ";
+    std::cout << "\nEdit:\t1 - food\n\t2 - danger\n\tEnter: ";
     std::cin >> c;
     switch (c)
     {
     case 1:
-    {   std::string place;
-        std::cout << "\nEnter new place: ";
+    {   std::string food;
+        std::cout << "\nEnter new food: ";
         std::cin.ignore();
-        getline(std::cin, place);
-        setPlace(place);}
+        getline(std::cin, food);
+        setFood(food);}
         break;
     case 2:
         int danger;
@@ -52,21 +52,12 @@ void Canines::edit()
 void Canines::save(std::ofstream& F)
 {
     saveAnimal(F);
-    F  << " place: " << getPlace() << "\t\tlevel of danger: " << getDanger() << std::endl;
+    F  << " food: " << getFood() << "\t\tlevel of danger: " << getDanger() << std::endl;
 }
 
-//void Canines::sort(const Canines& c1, const Canines& c2)
-//{
-//    sortByDanger(c1, c2);
-//}
-//
-//const Canines& Canines::sortByDanger(const Canines &c1, const Canines &c2)
-//{
-//    if (c1._dangerLevel > c2._dangerLevel)
-//        return c1;
-//    else
-//        return c2;
-//}
+
+
+
 
 
 
